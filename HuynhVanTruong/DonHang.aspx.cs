ad0d4655d5a5d6a68b1849f6a8d98d1829c97392
kimtu <kimtu@TRUONGIT>
2024-03-05 21:04:59 +0700

    Add project files.

---
+++ HuynhVanTruong/DonHang.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HuynhVanTruong
+{
+    public partial class DonHang : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                //khoi tao du lieu cho ddlBanh
+                ddlBanh.Items.Add(new ListItem("Bánh bò", "01"));
+                ddlBanh.Items.Add(new ListItem("Bánh mì", "02"));
+                ddlBanh.Items.Add(new ListItem("Hot dog", "03"));
+                ddlBanh.Items.Add(new ListItem("Bánh kem", "04"));
+            }
+        }
+
+        protected void btThem_Click(object sender, EventArgs e)
+        {
+            string data = $"{ddlBanh.SelectedItem.Text} ({txtSoLuong.Text})";
+            lstBanh.Items.Add(data);
+        }       
+
+        protected void btIn_Click(object sender, EventArgs e)
+        {
+            string kq = "";         
+            kq += "<h2 class='text-center'> HOÁ ĐƠN ĐẶT HÀNG </h2>";
+            kq += "<div class='border border-primary p-2'>";
+            kq += "Khách hàng: <i>" + txtHoTen.Text + "</i><br>";
+            kq += "Địa chỉ: <i>" + txtDiaChi.Text + "</i><br>";
+            kq += "Mã số thuế: <i>" + txtMST.Text + "</i><br><br>";
+
+            kq += "<b>Đặt các loại bánh sau:</b>";
+            kq += "<table class='table table-bordered'>";
+            char[] delim = { '(', ')' };
+            foreach (ListItem item in lstBanh.Items)
+            {
+                string[] arr = item.Text.Split(delim);
+                kq += "<tr>";
+                kq += $"<td> {arr[0]} </td> <td> {arr[1]} </td>";
+                kq += "</tr>";
+            }
+
+            kq += "</table>";
+            kq += "</div>";
+
+            //b2.gui thong tin ve client
+            lbKetQua.Text = kq;
+        }
+
+        protected void btnXoa_Click(object sender, EventArgs e)
+        {
+            for( int i = lstBanh.Items.Count - 1; i >= 0; i--)
+            {
+                if (lstBanh.Items[i].Selected)
+                {
+                    lstBanh.Items.RemoveAt(i);
+                }
+            }
+            
+        }
+    }
+}
